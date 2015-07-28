c6a499a165ae387099e6ae1098ab8a811cb682a6
Timur <murych@outlook.com>
2015-07-29 00:28:07 +0300

    clean up

--- source/main.R
+++ source/main.R
@@ -13,9 +13,3 @@ source('ui.R')
 initializing()
 
 init.ui()
-
-# mai.9 <- 'http://techcoockbooknotes.blogspot.com/2015/07/9-120304.html'
-# mai.6 <- 'http://techcoockbooknotes.blogspot.com/2015/07/6-120304.html'
-# 
-# grid.arrange(graph.create(table.sort(mai.6), number(mai.6)), 
-#              graph.create(table.sort(mai.9),number(mai.9)), ncol=2)
