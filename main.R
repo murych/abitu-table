69ec61fd38d9860516ba9677b99b5d4fcd629eea
Timur <murych@outlook.com>
2015-07-28 08:34:06 +0300

    v 0.4
    ui, which is not work at full power

--- main.R
+++ main.R
@@ -1,9 +1,19 @@
-source('functions.R')
+import <- function(){
+  library(XML)
+  library(RGtk2)
+  library(cairoDevice)
+  library(ggplot2)
+}
+
+
+source('ui.R')
 
 import()
 
-mai.9 <- 'http://techcoockbooknotes.blogspot.com/2015/07/9-120304.html'
-mai.6 <- 'http://techcoockbooknotes.blogspot.com/2015/07/6-120304.html'
+init.ui()
 
-grid.arrange(graph.create(table.sort(mai.6), number(mai.6)), 
-             graph.create(table.sort(mai.9),number(mai.9)), ncol=2)
+# mai.9 <- 'http://techcoockbooknotes.blogspot.com/2015/07/9-120304.html'
+# mai.6 <- 'http://techcoockbooknotes.blogspot.com/2015/07/6-120304.html'
+# 
+# grid.arrange(graph.create(table.sort(mai.6), number(mai.6)), 
+#              graph.create(table.sort(mai.9),number(mai.9)), ncol=2)
--- ui.R
+++ ui.R
@@ -1,10 +1,3 @@
-import <- function(){
-  library('RGtk2')
-  library('cairoDevice')
-  library(ggplot2)
-}
-
-
 init.ui <- function(){
   button.action <- function(button){
     source('mirea.R')
@@ -16,7 +9,7 @@ init.ui <- function(){
     gtkWidgetSetSizeRequest(plotArea, 400, 400)
     asCairoDevice(plotArea)
     gtkWidgetShow(plotArea)
-    dialog$add(plotArea)
+    new.window$add(plotArea)
     
     plot(table.mirea(url.mirea)$summ, type = 'l')
   }
@@ -27,11 +20,32 @@ init.ui <- function(){
   frame <- gtkFrameNew()
   window$add(frame)
   
-  box1 <- gtkVBoxNew()
-  box1$setBorderWidth(30)
+  box1 <- gtkHBoxNew(spacing = 10)
+  box1$setBorderWidth(85)
   frame$add(box1)
+  
+  university <- c("ÌÈÐÝÀ","ÌÀÈ #6", "ÌÀÈ #9", "ÌÈÝÒ")
+  
+  model.uni <-rGtkDataFrame(university)
+  combobox.uni <- gtkComboBox(model.uni)
+  crt.uni <- gtkCellRendererText()
+  combobox.uni$packStart(crt.uni)
+  combobox.uni$addAttribute(crt.uni, "text", 0)
+  gtkComboBoxSetActive(combobox.uni,0)
+  box1$packStart(combobox.uni)
+  
+  special <- c('12.03.04', '11.03.04', '11.03.03')
+  
+  model.dep <- rGtkDataFrame(special)
+  combobox.dep <- gtkComboBox(model.dep)
+  crt.dep <- gtkCellRendererText()
+  combobox.dep$packStart(crt.dep)
+  combobox.dep$addAttribute(crt.dep, "text", 0)
+  gtkComboBoxSetActive(combobox.dep,0)
+  box1$packStart(combobox.dep)
+
   
   GetResult <- gtkButton('Get Plot')
   gSignalConnect(GetResult, "clicked", button.action)
-  box1$packStart(GetResult, fill = T)
+  box1$packStart(GetResult, fill = F)
 }
